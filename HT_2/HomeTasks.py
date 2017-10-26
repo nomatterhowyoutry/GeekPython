6bf1c7bb5e5a03854e833fa48f7decbb9d0e9874
nomatterhowyoutry <nomatterhowyoutry@debian.debik>
2017-10-26 19:19:18 +0300

    Task1

---
+++ HT_2/HomeTasks.py
@@ -0,0 +1,23 @@
+def season1(n):
+    #season(1-12)
+    a = {'Winter': (1, 2, 12),'Spring': (3, 4, 5),'Summer': (6, 7, 8),'Autumn': (9, 10, 11)}
+    for k,v in a.items():
+        if n in v:
+            return k
+    return 'Wrong Number!'
+
+def season2(n):
+    #season(1-12)
+    if n == 1 or 2 or 12:
+        return 'Winter'
+    elif n == 3 or 4 or 5:
+        return 'Spring'
+    elif n == 6 or 7 or 8:
+        return 'Summer'
+    elif n == 9 or 10 or 11:
+        return 'Autumn'
+    return 'Wrong Number!'
+
+def season3(n):
+    #season(1-12)
+    return 'Wrong Number!'
