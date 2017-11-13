ea4348017819585b2eae911f32d8cc82fe1daac6
nomatterhowyoutry <nomatterhowyoutry@debian.debik>
2017-11-13 14:40:03 +0200

    made some improvments

--- HT_3/HomeTask3.py
+++ HT_3/HomeTask3.py
@@ -1,40 +1,45 @@
 import random
 import time
 
-iList = [random.randint(0, 99) for i in range(0, 100000)]
-fList = [random.uniform(0, 99) for i in range(0, 100000)]
+len = 10000
+iList = [random.randint(0, 99) for i in range(0, len)]
+fList = [random.uniform(0, 99) for i in range(0, len)]
 
 def Bubble_Sort(A):
     start = time.time()
-    for i in range(0, len(A)):
-        for j in range(i, len(A)):
+    for i in range(0, len):
+        for j in range(i, len):
             if A[i] > A[j]:
                 A[i], A[j] = A[j], A[i]
-    print('Bubble Sort time: {:.3f} sec'.format(time.time() - start), 'The list is sorted correctly =', (A == sorted(A)))
+    print('Bubble Sort time: {:.3f} sec'.format(time.time() - start), 'The list is sorted correctly = {}'.format(None == A.sort()))
     return A
 
 Bubble_Sort(iList)
 Bubble_Sort(fList)
+iList = [random.randint(0, 99) for i in range(0, len)]
+fList = [random.uniform(0, 99) for i in range(0, len)]
 
 def Selection_Sort(A):
     start = time.time()
-    for i in range(0, len(A)):
-        m = A.index(min(A[i:]),i,len(A))
+    for i in range(0, len):
+        m = A.index(min(A[i:]),i,len)
         A[m], A[i] = A[i], A[m]
-    print('Selection Sort time: {:.3f} sec'.format(time.time() - start), 'The list is sorted correctly =', (A == sorted(A)))
+    print('Selection Sort time: {:.3f} sec'.format(time.time() - start), 'The list is sorted correctly = {}'.format(None == A.sort()))
     return A
 
 Selection_Sort(iList)
 Selection_Sort(fList)
+iList = [random.randint(0, 99) for i in range(0, len)]
+fList = [random.uniform(0, 99) for i in range(0, len)]
 
 def Insertion_Sort(A):
     start = time.time()
-    for i in range(1, len(A)):
+    for i in range(1, len):
         j = i
         while (j > 0 and A[j] < A[j - 1]):
             A[j], A[j - 1] = A[j - 1], A[j]
             j = j - 1
-    print('Insertion Sort time: {:.3f} sec'.format(time.time() - start), 'The list is sorted correctly =', (A == sorted(A)))
+    print('Insertion Sort time: {:.3f} sec'.format(time.time() - start), 'The list is sorted correctly = {}'.format(None == A.sort()))
     return A
 
 Insertion_Sort(iList)
