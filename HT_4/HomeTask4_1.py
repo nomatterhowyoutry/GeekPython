1e963f23bf0a941201022490c68e370186f827ae
nomatterhowyoutry <nomatterhowyoutry@debian.debik>
2017-11-13 12:26:10 +0200

    Made few changes to optimize code

--- HT_4/HomeTask4_1.py
+++ HT_4/HomeTask4_1.py
@@ -1,19 +1,13 @@
 import re
 import csv
 
-f1 = open('openerp-server.log')
 pattern = re.compile('(.*\d.-\d.-\d.*\d.:\d.:\d.).*(ERROR|WARNING|CRITICAL) (\pg|\?|\None) (.*)\r')
+file1 = open('openerp-server.log', 'r')
+file2 = open('all_data.csv','wb')
+writer = csv.DictWriter(file2, fieldnames = ('line_id', 'marker', 'date_time', 'description'))
+writer.writeheader()
 
-ans = []
-with open('openerp-server.log', 'r') as file:
-        for line, text in enumerate(file):
-            if re.findall(pattern, text):
-                row = re.findall(pattern, text)
-                row = (line,) + row[0]
-                ans.append(row)
-
-with open('all_data.csv','wb') as file:
-    writer = csv.DictWriter(file, fieldnames = ('line_id', 'marker', 'date_time', 'description'))
-    writer.writeheader()
-    writer.writerows({'line_id': row[0], 'marker': row[2], 'date_time': row[1],
-                      'description': row[4]} for row in ans)
+for line, text in enumerate(file1):
+    if re.findall(pattern, text):
+        row = re.findall(pattern, text)
+        writer.writerow({'line_id': line, 'marker': row[0][1], 'date_time': row[0][0],'description': row[0][3]})
